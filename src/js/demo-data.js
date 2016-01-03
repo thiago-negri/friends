73f0e381026c1c21a3582de84f0bd284e783b630
Thiago B. Negri <evohunz@gmail.com>
2016-01-03 18:29:30 -0200

    Create fake data for testing

---
+++ src/js/demo-data.js
@@ -0,0 +1,23 @@
+var fran = {
+  name: 'Franciani Borghezan Negri',
+  dates: [
+    {
+      label: 'Aniversário',
+      date: '1993-02-04'
+    },
+    {
+      label: 'Casamento',
+      date: '2015-02-04'
+    }
+  ],
+  likes: [
+    'Cachorro',
+    'Artesanato',
+    'Cozinha',
+    'Pipoca'
+  ],
+  dislikes: [
+    'Lactose',
+    'Pedaços visíveis de cebola'
+  ]
+}
