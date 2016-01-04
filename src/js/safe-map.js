064a81aae5b7d05c8afd30ba8d11be8b035c2098
Thiago B. Negri <evohunz@gmail.com>
2016-01-04 13:45:35 -0200

    Add index argument to safeMap callback

--- src/js/safe-map.js
+++ src/js/safe-map.js
@@ -1,7 +1,12 @@
 function safeMap (array, callback) {
   'use strict'
+  var index = 0
   if (Array.isArray(array)) {
-    return array.map(callback)
+    return array.map(function (e) {
+      var currentIndex = index
+      index += 1
+      return callback(e, currentIndex)
+    })
   } else {
     return null
   }
