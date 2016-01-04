54913212e14f88328af8f13929769fb6b4aa6553
Thiago B. Negri <evohunz@gmail.com>
2016-01-04 13:47:41 -0200

    Add SCSS into the mix

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+# Compile SCSS files
+cd src/css
+for input_file in *.scss
+do
+  output_file="../../www/css/${input_file/scss/css}"
+  scss --force --no-cache --sourcemap=none $input_file $output_file
+done
+cd -
 
 # Compile JSX files
 jsx --no-cache-dir src/js www/js
